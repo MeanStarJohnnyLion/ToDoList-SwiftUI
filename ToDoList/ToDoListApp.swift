0a9508f6fe0e2384bbcbbe63429d9c98bb4b8387
Johnny Morgan <johnnylion3@aol.com>
2022-10-02 15:11:46 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Johnny Lion on 10/2/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
